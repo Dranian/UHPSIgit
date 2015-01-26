7b0cd7ba9271b0fa32bcd1ec1e073ac2f08ccc6c
Dranian <devin.routh@gmail.com>
2015-01-26 14:52:39 -0500

    Trying now

--- TestCode.R
+++ TestCode.R
@@ -3,3 +3,4 @@
 #I can see your code now! But I wasn't able to pull back changes in my other one.
 
 #Hmmm, trying again circa 1450
+#The program prompted me for my credentials; I'm trying again now.
